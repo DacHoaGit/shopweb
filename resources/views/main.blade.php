a1b39b737c6bde1ecab587556266e76f1e068d71
Lê Đắc Hòa <Ledachoa99@gmail.com>
2022-08-18 02:14:37 +0700

    Reformat

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -130,14 +130,6 @@
 			<div id="loadProduct">
 				@include('list')
 			</div>
-
-			<!-- Load more -->
-			<div class="flex-c-m flex-w w-full p-t-45 pointer" id="loadMore" >
-				<input type="hidden" value="1" id="page">
-				<a onclick="loadMore()" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
-					Load More
-				</a>
-			</div>
 		</div>
 	</section>
 
