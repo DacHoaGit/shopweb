ea0626da0b41a94adb2a7e1981505b57e6d794fb
Lê Đắc Hòa <Ledachoa99@gmail.com>
2022-08-28 15:38:12 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -215,6 +215,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,  
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ])->toArray(),
 
